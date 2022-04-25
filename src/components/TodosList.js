3ce28ceac24166e9ddbaed6019f0262ee35e843e
Augusto <jesus_augga_99@icloud.com>
2022-04-25 14:18:47 -0500

    add edit option to todos

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -3,14 +3,17 @@ import TodoItem from "./TodoItem";
 
 class TodosList extends React.Component {
   render() {
+    const { handleChangeProps, deleteTodoProps, setUpdate } = this.props
+
     return (
       <ul>
         {this.props.todos.map(todo => (
           <TodoItem
             key={todo.id}
             todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
+            handleChangeProps={handleChangeProps}
+            deleteTodoProps={deleteTodoProps}
+            setUpdate={setUpdate}
           />
         ))}
       </ul>
