07cfd8e1eff53d338bd216a363e4f39136532be8
Augusto <jesus_augga_99@icloud.com>
2022-04-25 12:22:58 -0500

    add delete button

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -22,12 +22,38 @@ class TodoContainer extends React.Component {
       }
     ]
    };
+
+   handleChange = id => {
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        }),
+      }
+    })
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
    
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     )
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,8 +7,14 @@ class TodoItem extends React.Component {
         <input
           type="checkbox"
           checked={this.props.todo.completed}
-          onChange={() => console.log("clicked")}
-        /> {this.props.todo.title}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        /> 
+        <button 
+          onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+        >
+          Delete
+        </button>
+        {this.props.todo.title}
       </li>
     )
   }
