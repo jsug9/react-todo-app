428f0d41416c1f13e09376ae93deafdbeea4d2eb
Augusto <jesus_augga_99@icloud.com>
2022-04-25 17:24:09 -0500

    add basename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 //component file
 import TodoContainer from "./components/TodoContainer"
@@ -10,7 +10,7 @@ import "./App.scss"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
