d3cb43c77c89813502f38abf7093830bd8d37021
Augusto <jesus_augga_99@icloud.com>
2022-04-25 10:23:28 -0500

    setup of the project

---
+++ src/components/TodosList.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class TodosList extends React.Component {
+  state = {};
+   
+  render() {
+    return (
+      <ul>
+        <li>Test</li>
+      </ul>
+    )
+  }
+}
+
+export default TodosList
