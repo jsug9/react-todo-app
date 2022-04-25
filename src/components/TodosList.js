2aef489404e4e56e73c6227c974bec2337938f38
Augusto <jesus_augga_99@icloud.com>
2022-04-25 15:46:00 -0500

    refactor everything to functions

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,24 +1,20 @@
 import React from "react"
 import TodoItem from "./TodoItem";
 
-class TodosList extends React.Component {
-  render() {
-    const { handleChangeProps, deleteTodoProps, setUpdate } = this.props
-
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={handleChangeProps}
-            deleteTodoProps={deleteTodoProps}
-            setUpdate={setUpdate}
-          />
-        ))}
-      </ul>
-    )
-  }
+const TodosList = props => {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+          setUpdate={props.setUpdate}
+        />
+      ))}
+    </ul>
+  )
 }
 
 export default TodosList
