a358393d2ef1551976afc49df0961ee04a61dfb7
Augusto <jesus_augga_99@icloud.com>
2022-04-25 13:05:53 -0500

    add style to the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from 'react-dom/client';
+//component file
 import TodoContainer from "./components/TodoContainer"
+
+//stylesheet
+import "./App.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
